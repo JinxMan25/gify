ac9646ed619e5dfa8bc6b7affa82b4c828d433e7
Md Samiul Huq <samiulg3@gmail.com>
2015-02-05 07:12:35 -0500

    added formidable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/get_video', function(req, res, next){
+  
 });
 
 module.exports = router;
