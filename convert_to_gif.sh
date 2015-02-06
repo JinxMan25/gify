fc64eb8212fbf58b987f65ee7e7c98ee55c225a3
Md Samiul Huq <samiulg3@gmail.com>
2015-02-06 18:53:32 -0500

    updated vars

--- convert_to_gif.sh
+++ convert_to_gif.sh
@@ -3,6 +3,13 @@
 cd "./temp_videos/"
 
 file=".gif"
+output=$1$file
+filename=$1
+
+startTime=$2
+endTime=$3
+
+res=$4
 
 ffmpeg -ss $startTime -i $filename -vf scale=$res:-1 -t $endTime -r 60 $output
 
