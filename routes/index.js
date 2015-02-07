c1ae30d4383c0e13a56bb8940236cf04b3aead04
Md Samiul Huq <samiulg3@gmail.com>
2015-02-06 19:15:01 -0500

    parse incoming video

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,18 @@ router.get('/', function(req, res) {
 });
 
 router.post('/get_video', function(req, res, next){
-  
+  var form = new formidable.IncomingForm();
+
+  form.parse(req, function(err,fields,files){
+    if (err){
+      return next(err);
+    }
+  });
+  form.on("end", function(fields,files){
+    if (this.openedFiles.length == 0){
+      return next(new Error ("You forgot the video!"));
+    }
+  });
 });
 
 module.exports = router;
