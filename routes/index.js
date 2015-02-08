c63f2747cfef30c103156726f013d2ef8f0bf51c
Md Samiul Huq <samiulg3@gmail.com>
2015-02-08 17:47:42 -0500

    exec shell script working

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,19 @@ router.post('/video', function(req, res, next){
     if (err){
       return next(err);
     }
-      console.log(files);
+    data['title'] = fields.title;
   });
+  console.log(data);
   form.on("end", function(fields, files){
     if (this.openedFiles.length == 0){
       return next(new Error ("You forgot the video!"));
     }
-    console.log(this.openedFiles);
+    var file = this.openedFiles[0].name;
     var date = Date.now();
     var video_file_loc = './static/temp/' + date + '-' + this.openedFiles[0].name;
     var gif_name = this.openedFiles[0].name.replace(/\.\w+/g, "");
     var tmp_loc = this.openedFiles[0].path;
+    var img_url = './static/images/' + date + '-' + gif_name;
 
     fs.copy(tmp_loc, video_file_loc,function(err){
       if (err){
@@ -36,7 +38,6 @@ router.post('/video', function(req, res, next){
       }
     });
   });
-    var img_url = './static/images/' + date + '-' + gif_name;
 
   /*form.on("end", function(fields,files){
     var tmp_loc = this.openedFiles[0].path;
@@ -49,5 +50,12 @@ router.post('/video', function(req, res, next){
     date['img_url'] = img_url;
   });*/
 });
+
+  router.get('/test', function(req,res,next){
+    child_process.exec('./test.sh', function(err, data){
+      console.log(err);
+      console.log(data);
+    });
+  });
 
 module.exports = router;
