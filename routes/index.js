b0d21f8911f5c36a071580c0398dd58f0d70fdf0
Md Samiul Huq <samiulg3@gmail.com>
2015-02-08 10:36:56 -0500

    upload working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var formidable = require('formidable');
 var child_process = require('child_process');
 var fs = require('fs-extra');
-var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,9 +11,43 @@ router.get('/', function(req, res) {
 
 router.post('/video', function(req, res, next){
   var form = new formidable.IncomingForm();
+
   form.parse(req, function(err,fields,files){
-    console.log(files);
+    if (err){
+      return next(err);
+    }
+      console.log(files);
   });
+  form.on("end", function(fields, files){
+    console.log(this.openedFiles);
+    var file = this.openedFiles[0].name;
+    var tmp_loc = this.openedFiles[0].path;
+
+    fs.copy(tmp_loc, './static/temp/' + file, function(err){
+      if (err){
+        console.log(err);
+      } else {
+        console.log(file + ' was uploaded ');
+      }
+    });
+  });
+  res.json("copied");
+
+  /*form.on("end", function(fields,files){
+    if (this.openedFiles.length == 0){
+      return next(new Error ("You forgot the video!"));
+    }
+    var tmp_loc = this.openedFiles[0].path;
+    var date = Date.now();
+    var file_name = this.openedFiles[0].name;
+    var img_url = 'static/images/' + date + '-' + file_name;
+    
+    child_process.exec('.././convert_to_gif.sh ../temp_videos/' + file_name + ' ' + res + ' ' + startTime + ' ' + remainingTime + ' ');
+
+    
+
+    date['img_url'] = img_url;
+  });*/
 });
 
 module.exports = router;
