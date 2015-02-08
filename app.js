603ccfe6e254829ac21e80c6dde3bdbeb6b5afbd
Md Samiul Huq <samiulg3@gmail.com>
2015-02-08 10:34:05 -0500

    startover

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var formidable = require('formidable');
 
 require('./models/Gif');
 var Gif = mongoose.model('Gif');
--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,18 @@ var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');
 var child_process = require('child_process');
+var fs = require('fs-extra');
+var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/get_video', function(req, res, next){
-  var data = {}
-  var startTime;
-  var resolution;
-  var remainingTime;
+router.post('/video', function(req, res, next){
   var form = new formidable.IncomingForm();
-
-
   form.parse(req, function(err,fields,files){
-    if (err){
-      return next(err);
-    } else {
-    startTime = fields.starttime;
-    remainingTime = fields.endtime;
-    resolution = fields.resolution;
-    }
-  });
-  form.on("end", function(fields,files){
-    if (this.openedFiles.length == 0){
-      return next(new Error ("You forgot the video!"));
-    }
-    var tmp_loc = this.openedFiles[0].path;
-    var date = Date.now();
-    var file_name = this.openedFiles[0].name;
-    var img_url = 'static/images/' + date + '-' + file_name;
-    
-    child_process.exec('.././convert_to_gif.sh ../temp_videos/' + file_name + ' ' + res + ' ' + startTime + ' ' + remainingTime + ' ';
-
-    
-
-    date['img_url'] = img_url;
+    console.log(files);
   });
 });
 
