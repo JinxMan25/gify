f5d5673da945d66ac07c2fd0d335590852b96ff3
Md Samiul Huq <samiulg3@gmail.com>
2015-02-08 20:25:58 -0500

    pass conditions for ffmpeg gif conversion

--- routes/index.js
+++ routes/index.js
@@ -22,13 +22,17 @@ router.post('/video', function(req, res, next){
     if (this.openedFiles.length == 0){
       return next(new Error ("You forgot the video!"));
     }
+    var startTime = "00:00:30";
+    var time = "5";
     var file = this.openedFiles[0].name;
+
     var date = Date.now();
+
     var video_file_loc = './static/temp/' + date + '-' + this.openedFiles[0].name;
-    var gif_name = this.openedFiles[0].name.replace(/\.\w+/g, "");
+    var gif_name = date + this.openedFiles[0].name.replace(/\.\w+/g, "");
     var tmp_loc = this.openedFiles[0].path;
     var img_url = './static/images/' + date + '-' + gif_name;
-    promiseCopy(tmp_loc, video_file_loc)
+    promiseCopy(tmp_loc, video_file_loc, startTime, time, gif_name)
 
 
   });
@@ -45,19 +49,19 @@ router.post('/video', function(req, res, next){
   });*/
 });
 
-var promiseCopy = function(tmp, new_loc){
+var promiseCopy = function(tmp, new_loc, beginning, length, name){
   fs.copy(tmp, new_loc,function(err){
     var deferred = Q.defer();
     if (err){
       console.log(err);
     } else {
       console.log('Uploaded');
-      convert();
+      convert(new_loc, beginning, length, name);
     }
   });
 }
-var convert = function(){
-  child_process.exec('ls', function(err, data){
+var convert = function(file, start, remaining, name){
+  child_process.exec('./convert_to_gif.sh ' + file + ' ' + start + ' ' + remaining + ' ' + name, function(err, data){
     if (err){
       console.log(err);
     } else {
